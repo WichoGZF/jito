8040e9a8f6fb7c9f2fc1947a6c08ff9f21b3ce7b
WichoGZF <wichogzf@gmail.com>
2022-04-25 21:33:57 -0500

    Updated gitignore

--- src/components/ResponsiveAppBar.js
+++ src/components/ResponsiveAppBar.js
@@ -26,42 +26,42 @@ const ResponsiveAppBar = (props) => {
     };
 
     const handleLogIn = () => {
-        if(props.loggedIn){
-            return(
+        if (props.loggedIn) {
+            return (
                 <Box sx={{ flexGrow: 0 }}>
-                <Tooltip title="Open settings">
-                    <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                        <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-                    </IconButton>
-                </Tooltip>
-                <Menu
-                    sx={{ mt: "45px" }}
-                    id="menu-appbar"
-                    anchorEl={anchorElUser}
-                    anchorOrigin={{
-                        vertical: "top",
-                        horizontal: "right"
-                    }}
-                    keepMounted
-                    transformOrigin={{
-                        vertical: "top",
-                        horizontal: "right"
-                    }}
-                    open={Boolean(anchorElUser)}
-                    onClose={handleCloseUserMenu}
-                >
-                    {settings.map((setting) => (
-                        <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                            <Typography textAlign="center">{setting}</Typography>
-                        </MenuItem>
-                    ))}
-                </Menu>
-            </Box>
+                    <Tooltip title="Open settings">
+                        <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                            <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                        </IconButton>
+                    </Tooltip>
+                    <Menu
+                        sx={{ mt: "45px" }}
+                        id="menu-appbar"
+                        anchorEl={anchorElUser}
+                        anchorOrigin={{
+                            vertical: "top",
+                            horizontal: "right"
+                        }}
+                        keepMounted
+                        transformOrigin={{
+                            vertical: "top",
+                            horizontal: "right"
+                        }}
+                        open={Boolean(anchorElUser)}
+                        onClose={handleCloseUserMenu}
+                    >
+                        {settings.map((setting) => (
+                            <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                                <Typography textAlign="center">{setting}</Typography>
+                            </MenuItem>
+                        ))}
+                    </Menu>
+                </Box>
             )
         }
-        else{
-            return(
-            <Button variant="contained">Log in</Button>
+        else {
+            return (
+                <Button variant="contained">Log in</Button>
             )
         }
     }
@@ -84,7 +84,7 @@ const ResponsiveAppBar = (props) => {
                         variant="h6"
                         noWrap
                         component="div"
-                        sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}
+                        sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
                     >
                         Pomodoro Pizza
                     </Typography>
