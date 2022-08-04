ea6c76e7f5f7ca79bd17fa9be2873f10c5bbc7b0
Luis Gustavo <wichogzf@gmail.com>
2022-08-04 18:06:05 -0500

    Calendar disabled when clock started or runnin

--- src/App.js
+++ src/App.js
@@ -7,63 +7,67 @@ import TaskList from './components/TaskList';
 import Box from "@mui/material/Box";
 import Grid from '@mui/material/Grid'
 
-import {format, compareAsc} from 'date-fns'
+import { format, compareAsc } from 'date-fns'
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { CalendarPicker } from '@mui/x-date-pickers/CalendarPicker';
 
 import { Divider } from '@mui/material';
 
-import {useDispatch, useSelector} from 'react-redux'
-import {setCalendarDate, startNewDay} from './features/appSlice.js'
+import { useDispatch, useSelector } from 'react-redux'
+import { setCalendarDate, startNewDay } from './features/appSlice.js'
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(false);
 
   const dispatch = useDispatch();
-  const date = useSelector((state) => state.app.calendarDate); 
+
+  const date = useSelector((state) => state.app.calendarDate);
+  const timerStarted = useSelector((state) => state.app.timerStarted);
+  const timerState = useSelector((state) => state.app.timerState);
 
   const dispatchCalendarChange = (newDate) => {
     dispatch(setCalendarDate(format(newDate, 'MM/dd/yyyy')))
-  } 
+  }
   //Converting from
   const dateDigits = date.split('/') //mm/dd/yyyy
   const [month, day, year] = dateDigits
-  const dateInDateType = new Date(year, month-1, day)
+  const dateInDateType = new Date(year, month - 1, day)
 
-  useEffect(()=> {
+  useEffect(() => {
     const hours = dateInDateType.getHours()
     const minutes = dateInDateType.getMinutes()
 
-    const actualMinutesInDay = hours*60 + minutes
-    const remainingMinutes = 24^60 - actualMinutesInDay
+    const actualMinutesInDay = hours * 60 + minutes
+    const remainingMinutes = 24 ^ 60 - actualMinutesInDay
     const remainingMiliseconds = remainingMinutes * 60 * 1000
-    setTimeout(()=>{
+    setTimeout(() => {
       dispatch(startNewDay())
     }, remainingMiliseconds)
   }, [])
 
   return (
-    <Box sx={{minWidth:'700px'}}>
+    <Box sx={{ minWidth: '700px' }}>
       <ResponsiveAppBar loggedIn={loggedIn}></ResponsiveAppBar>
-      <Grid container spacing={4} sx={{ marginTop: '5px'}}>
+      <Grid container spacing={4} sx={{ marginTop: '5px' }}>
         <Grid container item
           xs="auto"
           spacing={3}
           direction='column'
           justifyContent="flex-start"
           alignItems="stretch"
-          sx={{ pl: 0, pt: 0, position:'sticky'}}>
-          
-          <Grid item sx={{width:'100%'}}>
+          sx={{ pl: 0, pt: 0, position: 'sticky' }}>
+
+          <Grid item sx={{ width: '100%' }}>
             <TimerControl></TimerControl>
           </Grid>
           <Grid item>
             <Divider></Divider>
           </Grid>
-          <Grid item sx={{width:'100%'}}>
+          <Grid item sx={{ width: '100%' }}>
             <LocalizationProvider dateAdapter={AdapterDateFns}>
-              <CalendarPicker disablePast date={dateInDateType} onChange={dispatchCalendarChange}/> 
+              <CalendarPicker disablePast date={dateInDateType} onChange={dispatchCalendarChange}
+                disabled={timerState || timerStarted} />  
             </LocalizationProvider>
 
           </Grid>
