8f84f15e45fb8a419cb0ddd123e18e0b87f4ae26
Luis Gustavo <wichogzf@gmail.com>
2022-08-05 17:58:13 -0500

    Added local storage via redux-persist

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
-import store from './store.js'
+import {store, persistor} from './store.js'
 import { Provider } from 'react-redux';
-
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { PersistGate } from 'redux-persist/integration/react';
 
 const theme = createTheme({
   palette: {
@@ -24,11 +24,13 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <DndProvider backend={HTML5Backend}>
-        <ThemeProvider theme={theme}>
-          <App />
-        </ThemeProvider>
-      </DndProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <DndProvider backend={HTML5Backend}>
+          <ThemeProvider theme={theme}>
+            <App />
+          </ThemeProvider>
+        </DndProvider>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/store.js
+++ src/store.js
@@ -1,15 +1,36 @@
-import {configureStore} from '@reduxjs/toolkit'
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
 
 import authReducer from './features/authSlice.js'
 import settingsReducer from './features/settingsSlice.js'
 import tasksReducer from './features/tasksSlice.js'
 import appReducer from './features/appSlice.js'
 
-export default configureStore({
-    reducer: {
-        auth: authReducer,
-        settings: settingsReducer,
-        tasks: tasksReducer,
-        app: appReducer
-    },
+import storage from 'redux-persist/lib/storage'
+import storageSession from 'redux-persist/lib/storage/session'
+import { persistReducer, persistStore } from 'redux-persist'
+
+const rootPersistConfig = {
+    key: 'root',
+    storage,
+    blacklist: ['auth']
+}
+
+const appPersistConfig = {
+    key: 'app', 
+    storage: storageSession,
+}
+
+const rootReducer = combineReducers({
+    auth: authReducer,
+    settings: settingsReducer,
+    tasks: tasksReducer,
+    app: persistReducer(appPersistConfig, appReducer)
+})
+
+const persistedReducer = (persistReducer(rootPersistConfig, rootReducer))
+
+export const store = configureStore({
+    reducer: persistedReducer
 }) 
+
+export const persistor = persistStore(store)
