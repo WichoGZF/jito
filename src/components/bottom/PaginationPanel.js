b3dc6b2a4b112a5879f732c4975c02be63cfb432
WichoGZF <wichogzf@gmail.com>
2023-01-24 20:06:58 -0600

    Fixed typos in pagination text

--- src/components/bottom/PaginationPanel.js
+++ src/components/bottom/PaginationPanel.js
@@ -7,26 +7,26 @@ const paginationContent = [
     {
         title: `What is it`,
         content: `Pomodoro Planner is a time tracking and day planning app that allows you to
-        plan your entire day and future days, based on a Pomodoro work-rest scheduled.`
+        plan your days, based on pomodoro blocks.`
     },
     {
         title: `Types of tasks`,
         content: `
         Pomodoro planner uses two main tasks, 'block' and 'normal':\n
-        -Block tasks have a fixed ammount of pomodoros and are represented by having a 'box' on their left side.
-        You complete them by doing pomodoros until they reach 0. Good for things you only want to spend a fixed ammount on
+        Block tasks have a fixed ammount of pomodoros and are represented by having a 'box' on their left side.
+        You complete them by doing pomodoros until they reach 0. Good for things you only want to spend a fixed ammount of time on.
         \n
-        -Normal tasks have a clickable circle on the left. Clicking the circle completes the task.
+        Normal tasks have a clickable circle on the left. Clicking the circle completes the task.
         They need to be at the top to be 'completed'. Normal tasks have no time limit, you have to click on the circle to
-        complete them. If you complete a normal task while the timer has started (before the break starts) an according
-        ammount of break time is calculated and you can choose to use it now (break starts) store it for later (next break
-        the stored time is used) or discard it. 
+        complete them. If you complete a normal task while the timer has started (before the break starts) and according to the 
+        ammount of time you've spent on the pomodoro, a corresponding ammount of break time is calculated and you can choose to use
+         it now (break starts), store it for later (next break the stored time is used) or discard it. 
         `
     },
     {
         title: `Calendar feature`,
         content: `
-        Pomodoro Planner uses the calendar below the timer to plan ahead your tasks. Just click a specific date in the
+        Pomodoro Planner uses a calendar (below the timer) to plan ahead your tasks. Just click a specific date in the
         calendary and you can see your scheduled tasks for that day or add new ones in advance.
         `
     },
