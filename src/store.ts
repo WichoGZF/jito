03bbb49b97da264a6c3f51e2ea863b55013f072f
WichoGZF <wichogzf@gmail.com>
2023-04-23 23:17:05 -0600

    Removed redundant auth slice

--- src/store.ts
+++ src/store.ts
@@ -1,51 +1,26 @@
-import { configureStore, combineReducers, getDefaultMiddleware } from '@reduxjs/toolkit'
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
 
-import authReducer from './features/authSlice'
 import settingsReducer from './features/settingsSlice'
 import tasksReducer from './features/tasksSlice'
 import appReducer from './features/appSlice'
 
-import storage from 'redux-persist/lib/storage'
-import storageSession from 'redux-persist/lib/storage/session'
-import {
-    persistStore,
-    persistReducer,
-    FLUSH,
-    REHYDRATE,
-    PAUSE,
-    PERSIST,
-    PURGE,
-    REGISTER,
-  } from 'redux-persist'
+import { apiSlice } from 'features/api/apiSlice'
 
-const rootPersistConfig = {
-    key: 'root',
-    storage,
-    blacklist: ['auth']
-}   
 
 const rootReducer = combineReducers({
-    auth: authReducer,
     settings: settingsReducer,
     tasks: tasksReducer,
     app: appReducer,
+    [apiSlice.reducerPath]: apiSlice.reducer
 })
 
-const persistedReducer = (persistReducer(rootPersistConfig, rootReducer))
-
 export const store = configureStore({
-    reducer: persistedReducer,
+    reducer: rootReducer,
     middleware: (getDefaultMiddleware) => 
-    getDefaultMiddleware({
-        serializableCheck: {
-            ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-        },
-    }),
+    getDefaultMiddleware().concat(apiSlice.middleware),
 })  
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch
-
-export const persistor = persistStore(store)
