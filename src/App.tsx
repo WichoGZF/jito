e2385122032978f31b09a93093cf9efcc2cfd2bc
WichoGZF <wichogzf@gmail.com>
2023-05-02 16:05:45 -0600

    GetUserData fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,58 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef, useContext } from 'react';
 import ResponsiveAppBar from "./components/navbar/ResponsiveAppBar";
 import TaskList from './components/tasks/TaskList';
 import Grid from '@mui/material/Grid'
 import { startNewDay } from './features/appSlice'
 import { ThemeProvider, responsiveFontSizes } from "@mui/material/styles";
 import Calendar from './components/timerSection/Calendar';
-import { Container, Divider, Stack, Box} from '@mui/material';
+import { Container, Divider, Stack, Box } from '@mui/material';
 import Footer from 'components/bottom/Footer';
 import PaginationPanel from 'components/bottom/PaginationPanel';
 import midnightWorker from './midnightWorker'
 import { lightTheme, darkTheme } from 'theme';
-import { format } from 'date-fns';  
+import { format } from 'date-fns';
 import { useAppSelector, useAppDispatch } from 'hooks';
 import Subscribe from 'components/subscription/Subscribe';
+import { useLazyGetUserDataQuery } from 'features/api/apiSlice';
+import { updateSettings } from 'features/settingsSlice';
+import { updateTaskSlice } from 'features/tasksSlice';
 
 const composeResetDay = () => (dispatch, getState) => {
   dispatch(startNewDay(getState().settings.pomodoroDuration))
 }
 
 const milisecondsInDay = (24 * 60 * 60 * 1000)
- 
+
 function App() {
   const [loggedIn, setLoggedIn] = useState<boolean>(false);
   const [subscribeDialogOpen, setSusbcribeDialogOpen] = useState<boolean>(false);
 
+  const hasSession = useAppSelector(state => state.auth.hasSession)
+  const userid = useAppSelector(state => state.auth.userid)
+
+  const [getUserData, result] = useLazyGetUserDataQuery()
+
+  useEffect(() => {
+    if (hasSession && (userid !== null)) {
+      getUserData(userid)
+    }
+
+  }, [hasSession])
+
+  useEffect(() => { 
+    if(result.data){ 
+      dispatch(updateSettings(result.data.settings))
+      dispatch(updateTaskSlice({
+        tasks: result.data.tasks,
+        tags: result.data.tags,
+        history: result.data.historicTask
+      }))
+    }
+  }, [result])
+
+
   const handleCloseSubscripe = () => {
-    setSusbcribeDialogOpen(false); 
+    setSusbcribeDialogOpen(false);
   }
 
   const dispatch = useAppDispatch();
@@ -45,20 +72,20 @@ function App() {
 
   //UseEffect to check if the day has been shown yet 
   useEffect(() => {
-      //Fetch actual date to see if todayDate is outdated
-      const actualDate = format(new Date(), 'MM/dd/yyyy') 
-      if(actualDate !== todayDate){
-        dispatch(composeResetDay());
-      }
+    //Fetch actual date to see if todayDate is outdated
+    const actualDate = format(new Date(), 'MM/dd/yyyy')
+    if (actualDate !== todayDate) {
+      dispatch(composeResetDay());
+    }
   }, [])
 
   useEffect(() => {
-    function eventHandler(){ 
+    function eventHandler() {
       console.log('New day started');
       dispatch(composeResetDay());
     }
     //Actual timestamp
-    const timestamp = new Date();  
+    const timestamp = new Date();
     //Timestamp converted to miliseconds 
     const timestampMiliseconds = (timestamp.getHours() * 60 * 60 * 1000) + (timestamp.getMinutes() * 60 * 1000) + (timestamp.getSeconds() * 1000);
 
@@ -75,10 +102,10 @@ function App() {
   }, [todayDate])
 
   let theme;
-  if (colorTheme === 'dark'){
+  if (colorTheme === 'dark') {
     theme = darkTheme
   }
-  else{
+  else {
     theme = lightTheme
   }
 
@@ -86,7 +113,7 @@ function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Box sx={{backgroundColor: theme.palette.background.default}}>
+      <Box sx={{ backgroundColor: theme.palette.background.default }}>
         <ResponsiveAppBar loggedIn={loggedIn}></ResponsiveAppBar>
         <Container>
           <Stack
