6be76132f319f83677ac9cbd8444f8c9d92ecd29
WichoGZF <wichogzf@gmail.com>
2023-05-02 16:06:16 -0600

    removed persistor

--- src/index.tsx
+++ src/index.tsx
@@ -3,21 +3,18 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
-import { store, persistor } from './store'
+import { store } from './store'
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
 import './index.css'
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
         <DndProvider backend={HTML5Backend}>
           <App />
         </DndProvider>
-      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
