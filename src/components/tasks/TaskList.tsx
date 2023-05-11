189a0ba970f047410d70fc90a7d8d313e256b699
WichoGZF <wichogzf@gmail.com>
2023-05-10 20:13:30 -0600

    tidied up TaskList, moved functions up

--- src/components/tasks/TaskList.tsx
+++ src/components/tasks/TaskList.tsx
@@ -1,27 +1,53 @@
-import * as React from 'react';
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import { Typography } from '@mui/material';
 import { Grid } from '@mui/material';
 import { Box } from '@mui/system';
 import { Divider } from '@mui/material';
 import {
-  restartTask,
-} from '../../features/tasksSlice'
-import {
-  currentTag, currentIndex, currentType, initialize
+  currentTag, currentIndex, currentType
 } from '../../features/appSlice'
 import CompletedDialog from './CompletedDialog'
 import OverdueTaskList from './OverdueTaskList';
 import ListEntry from './ListEntry';
 import NewTask from './NewTask';
-
 import Task from '../../types/Task'
-
 import { useAppSelector } from 'hooks/useAppSelector'
-import {useAppDispatch} from 'hooks/useAppDispatch'
+import { useAppDispatch } from 'hooks/useAppDispatch'
 import Tag from 'types/Tag';
-import { isBefore } from 'date-fns';
 import useInitializeTasks from 'hooks/useInitializeTasks';
+
+function taskToListEntry(task: Task, firstTask: boolean, index: number, tags: Tag[]) {
+  return (
+    <ListEntry
+      key={task.tag + task.id}
+      id={task.id}
+      text={task.name}
+      description={task.description}
+      index={index}
+      date={task.date}
+      type={task.type}
+      blocks={task.blocks}
+      repeat={task.repeat}
+      repeatOn={task.repeatOn}
+      tag={task.tag}
+      tags={tags}
+      firstTask={firstTask}
+      completed={task.completed}
+    ></ListEntry>
+  )
+}
+interface FirstTaskInfo {
+  index: number,
+  tag: string,
+  type: 'normal' | 'block'
+}
+//Returns int representing the day of the week today.
+function getDayOfTheWeek(date: string): number {
+  const splitCalendarDate = date.split('/')
+  const [month, calendarDay, year] = splitCalendarDate
+  const realCalendarDate = new Date(parseInt(year), parseInt(month) - 1, parseInt(calendarDay))
+  return realCalendarDate.getDay();
+}
 
 export default function TaskList() {
   const dispatch = useAppDispatch()
@@ -36,86 +62,48 @@ export default function TaskList() {
 
   const [overdueNormals] = useInitializeTasks(tasks, initialized)
 
-  function taskToListEntry(task: Task, firstTask: boolean, index: number) {
-    return (
-      <ListEntry
-        key={task.tag + task.id}
-        id = {task.id}
-        text={task.name}
-        description={task.description}
-        index={index}
-        date={task.date}
-        type={task.type}
-        blocks={task.blocks}
-        repeat={task.repeat}
-        repeatOn={task.repeatOn}
-        tag={task.tag}
-        tags={tags}
-        firstTask={firstTask}
-        completed={task.completed}
-      ></ListEntry>
-    )
-  }
-  const splitCalendarDate = calendarDate.split('/')
-  const [month, calendarDay, year] = splitCalendarDate
-  const realCalendarDate = new Date(parseInt(year), parseInt(month) - 1, parseInt(calendarDay))
-  const dayOfTheWeek = realCalendarDate.getDay();
+  const dayOfTheWeek = getDayOfTheWeek(todayDate)
 
   let allTagTasks: any[] = [] //React array
   // For overDue normals dialog  //
-  let firstTaskIndex: number
-  let firstTaskTag: string
-  let firstTaskType: 'normal' | 'block'
-  //
-  //Loop for mapping tasks into components to show.
-  let firstTaskAdded = false
-  tasks.forEach((task, index) => {
+  let firstTaskInfo: FirstTaskInfo
 
+  //Loop for mapping tasks into components to show.
+  tasks.forEach((task, index) => {
     if (task.repeat === 'no-repeat') {
       if (task.date === calendarDate) {
         if (!allTagTasks.length) {
-          firstTaskAdded = true
+          firstTaskInfo = { index: index, tag: task.tag, type: task.type }
         }
-        allTagTasks.push(taskToListEntry(task, firstTaskAdded, index))
+        allTagTasks.push(taskToListEntry(task, !allTagTasks.length, index, tags))
       };
     }
     else {
       if (!task.completed || todayDate !== calendarDate) { //In order to filter completed yet not discard future tasks
-        switch (task.repeat) {
-          case 'weekly':
-            if (task.repeatOn[dayOfTheWeek]) {
-              if (!allTagTasks.length) {
-                firstTaskAdded = true
-              }
-              allTagTasks.push(taskToListEntry(task, firstTaskAdded, index))
-            }
-            break;
-          case 'daily':
+        if (task.repeat === 'weekly') {
+          if (task.repeatOn[dayOfTheWeek]) {
             if (!allTagTasks.length) {
-              firstTaskAdded = true
+              firstTaskInfo = { index: index, tag: task.tag, type: task.type }
             }
-            allTagTasks.push(taskToListEntry(task, firstTaskAdded, index));
-            break;
+            allTagTasks.push(taskToListEntry(task, !allTagTasks.length, index, tags))
+          }
+        }
+        else if (task.repeat === 'daily') {
+          if (!allTagTasks.length) {
+            firstTaskInfo = { index: index, tag: task.tag, type: task.type }
+          }
+          allTagTasks.push(taskToListEntry(task, !allTagTasks.length, index, tags));
         }
       }
-    }
-
-    if (firstTaskAdded) {//if this is the first task added
-      firstTaskIndex = index
-      firstTaskTag = task.tag
-      firstTaskType = task.type
-      //No longer the first task added
-      firstTaskAdded = false
-
     }
   })
 
   //Dispatches the information about the current tag to appSlice.
   useEffect(() => {
     if (allTagTasks.length) {
-      dispatch(currentTag(firstTaskTag));
-      dispatch(currentIndex(firstTaskIndex));
-      dispatch(currentType(firstTaskType));
+      dispatch(currentTag(firstTaskInfo.tag));
+      dispatch(currentIndex(firstTaskInfo.index));
+      dispatch(currentType(firstTaskInfo.type));
     }
   })
 
