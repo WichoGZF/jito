a490beb193348c8a150603c70cef2fd68a11b383
WichoGZF <wichogzf@gmail.com>
2023-05-16 23:43:05 -0600

    Component for login in

--- src/components/navbar/LogIn/LogIn.tsx
+++ src/components/navbar/LogIn/LogIn.tsx
@@ -1,40 +1,129 @@
-import { Grid, DialogContent, TextField, Button, Divider, SvgIcon, FormControl, Typography } from "@mui/material";
-import { Google, Facebook } from "@mui/icons-material";
-import FormGroup from "@mui/material/FormGroup/FormGroup";
+import { Grid, DialogContent, TextField, Button, Divider } from "@mui/material";
+import * as react from 'react'
+import { useState } from "react"
+import googleLogo from "assets/Google__G__Logo.svg"
+import facebookLogo from "assets/facebook_logo.webp"
+import { useAppDispatch } from "hooks/useAppDispatch"
+import { useAppSelector } from "hooks/useAppSelector"
+import { validateUser } from "features/authActions";
 interface Props {
     handleSetRegister: () => void,
 }
 
 function Login({ handleSetRegister }: Props) {
+    const [username, setUsername] = useState<string>("");
+    const [usernameError, setUsernameError] = useState<null | string>(null);
+    const [password, setPassword] = useState<string>("")
+    const [passwordError, setPasswordError] = useState<null | string>(null)
+
+    const loginError = useAppSelector(state => state.auth.loginError)
+    let loginErrorTxt = ""
+    if(loginError){
+        loginErrorTxt = "Incorrect username or password"
+    }
+
+    const dispatch = useAppDispatch()
+
+    const usernameHasError = () => {
+        if (username.length < 5) {
+            setUsernameError("Username must be between 5 to 15 characters.")
+            return true
+        }
+        else if (usernameError !== null){
+            setUsernameError(null)
+            return false
+        }
+    }
+
+    const passwordHasError = () => {
+        if (password.length < 8) {
+            setPasswordError("Password must be between 8 to 15 characters.")
+            return true
+        }
+        else if (passwordError !== null){
+            setPasswordError(null)
+            return false
+        }
+    }
+
+    function handleSubmit(e: any) {
+        e.preventDefault()
+        const userErr = usernameHasError()
+        const passErr = passwordHasError()
+        if (userErr || passErr) {
+            return
+        }
+        else{
+            dispatch(validateUser({username: username, password: password}))
+        }
+    }
+
     return (
         <DialogContent>
             <Grid container spacing={5} direction="row" >
                 <Grid item xs>
-                    <form>
-                            <Grid container spacing={2} direction="column" alignItems="expand">
-                                <Grid item>
-                                    <TextField inputProps={{ maxLength: 15 }} placeholder="Username" required name="user"></TextField>
-                                </Grid>
-                                <Grid item>
-                                    <TextField inputProps={{ maxLength: 15 }} placeholder="Password" required name="password"></TextField>
-                                </Grid>
-                                <Grid item xs>
-                                    <Button variant="contained" fullWidth={true} type='submit'>Log in</Button>
-                                </Grid>
-                                <Grid item xs>
-                                    <Button>Forgot your password?</Button>
-                                </Grid>
+                    <form onSubmit={handleSubmit}>
+                        <Grid container spacing={2} direction="column" alignItems="expand">
+                            <Grid item>
+                                <TextField
+                                    error={usernameError !== null || loginError}
+                                    helperText={loginError? loginErrorTxt : usernameError}
+                                    inputProps={{ maxLength: 15 }}
+                                    placeholder="Username"
+                                    required
+                                    name="user"
+                                    value={username}
+                                    onChange={(event: React.ChangeEvent<HTMLInputElement>) => { 
+                                        setUsername(event.target.value)
+                                        if (event.target.value.length < 5) {
+                                            setUsernameError("Username must be between 5 to 15 characters.")
+                                        }
+                                        else { 
+                                            setUsernameError(null)
+                                        }
+                                    }}
+                                    >
+       
+                                </TextField>
                             </Grid>
+                            <Grid item>
+                                <TextField
+                                    error={passwordError !== null || loginError}
+                                    type="password"
+                                    helperText={loginError? '' : passwordError}
+                                    inputProps={{ maxLength: 15 }}
+                                    placeholder="Password"
+                                    required
+                                    name="password"
+                                    value={password}
+                                    onChange={(event: React.ChangeEvent<HTMLInputElement>) => { 
+                                        setPassword(event.target.value)
+                                        if (event.target.value.length < 8) {
+                                            setPasswordError("Password must be between 8 to 15 characters.")
+                                        }
+                                        else { 
+                                            setPasswordError  (null)
+                                        }
+                                    }}
+                                    ></TextField>
+                            </Grid>
+                            <Grid item xs>
+                                <Button variant="contained" fullWidth={true} type='submit'>Log in</Button>
+                            </Grid>
+                            <Grid item xs>
+                                <Button>Forgot your password?</Button>
+                            </Grid>
+                        </Grid>
                     </form>
                 </Grid>
                 <Divider orientation="vertical" flexItem sx={{ pl: "40px" }}></Divider>
                 <Grid item xs>
                     <Grid container spacing={2} direction="column">
                         <Grid item>
-                            <Button variant="outlined" fullWidth={true} startIcon={<SvgIcon component={Google}></SvgIcon>}>Sign in with Google</Button>
+                            <Button variant="outlined" fullWidth={true} startIcon={<img style={{ width: '20px', height: '20px' }} src={googleLogo} />}>Sign in with Google</Button>
                         </Grid>
                         <Grid item>
-                            <Button variant="outlined" fullWidth={true} startIcon={<SvgIcon component={Facebook}></SvgIcon>}>Continue with Facebook</Button>
+                            <Button variant="outlined" fullWidth={true} startIcon={<img style={{ width: '20px', height: '20px' }} src={facebookLogo} />}>Continue with Facebook</Button>
                         </Grid>
                         <Grid item>
                             <Button fullWidth={true} onClick={handleSetRegister}>Don't have an account? Register</Button>
