223250fec77afafed034a93756c7c933cf342e63
WichoGZF <wichogzf@gmail.com>
2023-05-16 23:42:40 -0600

    Refactor

--- src/components/navbar/ResponsiveAppBar.tsx
+++ src/components/navbar/ResponsiveAppBar.tsx
@@ -1,4 +1,3 @@
-import * as React from "react";
 import { useState } from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
@@ -8,34 +7,19 @@ import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import AvTimerIcon from "@mui/icons-material/AvTimer";
 import Button from '@mui/material/Button';
-
-import { useAppSelector } from "hooks";
-
+import Stack from "@mui/material/Stack";
+import { useAppSelector } from "hooks/useAppSelector";
 import DialogSettings from './DialogSettings'
 import DialogLogIn from './DialogLogIn'
 import DialogStatistics from "./DialogStatistics";
+import DialogRegister from "./DialogRegister";
 import ButtonGroup from "@mui/material/ButtonGroup";
+import DialogLogOff from "./DialogLogOff";
 
-import { useTheme } from '@mui/material/styles';
-
-interface PropTypes{ 
-    loggedIn: boolean;
-}
-
-const ResponsiveAppBar = (props: PropTypes) => {
+const ResponsiveAppBar = () => {
     const [dialogOpen, setDialogOpen] = useState<number>(0)
 
-    const theme = useTheme()
-
-    const colorTheme = useAppSelector((state) => state.settings.colorTheme)
-
-    let loggedInLabel
-    if (props.loggedIn) {
-        loggedInLabel = "Logout"
-    }
-    else {
-        loggedInLabel = "Log In"
-    }
+    const hasSession = useAppSelector((state) => state.auth.hasSession)
 
     const handleOpenDialog = (index: number) => {
         setDialogOpen(index);
@@ -45,11 +29,25 @@ const ResponsiveAppBar = (props: PropTypes) => {
         setDialogOpen(0)
     }
 
+    let buttonGroup
+    if (hasSession) {
+        buttonGroup = <ButtonGroup variant={'text'} aria-label="outlined primary button group">
+            <Button onClick={() => handleOpenDialog(2)}>Statistics</Button>
+            <Button onClick={() => handleOpenDialog(1)}>Settings</Button>
+            <Button onClick={() => handleOpenDialog(5)}>Sign out</Button>
+        </ButtonGroup>
+    }
+    else {
+
+        buttonGroup = <Stack direction="row" gap={2}>
+            <Button variant="outlined" onClick={() => handleOpenDialog(3)}>Sign In</Button>
+            <Button variant="contained" onClick={() => handleOpenDialog(4)}>Sign Up</Button>
+        </Stack>
+    }
     return (
         <AppBar position="sticky" color='secondary'>
             <Container maxWidth="xl">
                 <Toolbar disableGutters>
-
                     <IconButton
                         size="large"
                         aria-label="account of current user"
@@ -59,7 +57,6 @@ const ResponsiveAppBar = (props: PropTypes) => {
                     >
                         <AvTimerIcon />
                     </IconButton>
-
                     <Typography
                         variant="h6"
                         noWrap
@@ -69,17 +66,15 @@ const ResponsiveAppBar = (props: PropTypes) => {
                         Pomodoro Planner
                     </Typography>
                     <Box sx={{ flexGrow: 0 }}>
-                        <ButtonGroup variant={'text'} aria-label="outlined primary button group">
-                            <Button onClick={() => handleOpenDialog(2)}>Statistics</Button>
-                            <Button onClick={() => handleOpenDialog(1)}>Settings</Button>
-                            <Button onClick={() => handleOpenDialog(3)}>Log In</Button>
-                        </ButtonGroup>
+                        {buttonGroup}
                     </Box>
                 </Toolbar>
             </Container>
             <DialogSettings open={dialogOpen === 1} handleClose={handleCloseDialog}></DialogSettings>
             <DialogStatistics open={dialogOpen === 2} handleClose={handleCloseDialog}></DialogStatistics>
             <DialogLogIn open={dialogOpen === 3} handleClose={handleCloseDialog}></DialogLogIn>
+            <DialogRegister open={dialogOpen === 4} handleClose={handleCloseDialog}></DialogRegister>
+            <DialogLogOff open={dialogOpen === 5} handleClose={handleCloseDialog}></DialogLogOff>
         </AppBar>
     );
 };
