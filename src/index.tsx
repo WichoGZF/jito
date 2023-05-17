3d6567476f65e9c28b2c3e6d85d712bcc36aefe7
WichoGZF <wichogzf@gmail.com>
2023-05-16 23:41:57 -0600

    Added react-cookie

--- src/index.tsx
+++ src/index.tsx
@@ -6,15 +6,18 @@ import { HTML5Backend } from 'react-dnd-html5-backend';
 import { store } from './store'
 import { Provider } from 'react-redux';
 import './index.css'
+import { CookiesProvider } from 'react-cookie';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+      <CookiesProvider>
         <DndProvider backend={HTML5Backend}>
           <App />
         </DndProvider>
+      </CookiesProvider>
     </Provider>
   </React.StrictMode>
 );
