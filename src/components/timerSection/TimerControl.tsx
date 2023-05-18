ec3293e3d95cd58e93c1ef16cc6e396c41601de0
WichoGZF <wichogzf@gmail.com>
2023-05-18 17:33:18 -0600

    Implemented create histask hook

--- src/components/timerSection/TimerControl.tsx
+++ src/components/timerSection/TimerControl.tsx
@@ -2,12 +2,11 @@ import { useState } from 'react';
 import TimerCard from './TimerCard';
 import { useAppDispatch } from "hooks/useAppDispatch"
 import { useAppSelector } from "hooks/useAppSelector"
-import { addTimeEntry } from '../../features/tasksSlice';
 import { stopRunning } from '../../features/appSlice'
 import { updateBlocks } from '../../features/tasksSlice';
-import { usePostHistoricTaskMutation } from 'features/api/apiSlice';
 import DialogSkipWarning from './DialogSkipWarning';
 import useTimerControl from 'hooks/useTimerControl';
+import useHandleCreateHistoric from 'hooks/useHandleCreateHistoric';
 
 /* TIMER CONTROL  
   Component handling logic for timer.
@@ -15,10 +14,8 @@ import useTimerControl from 'hooks/useTimerControl';
 
 export default function TimerControl() {
   //Random settings
-  const settings = useAppSelector(state => state.settings)
-  const actualTag = useAppSelector(state => state.app.tag)
+  const longBreakEvery = useAppSelector(state => state.settings.longBreakEvery)
   const actualIndex = useAppSelector(state => state.app.index)
-  const todayDate = useAppSelector(state => state.app.todayDate)
   //TimerState
   const timerMinuts = useAppSelector(state => state.app.minutes)
   const timerSeconds = useAppSelector(state => state.app.seconds)
@@ -27,33 +24,9 @@ export default function TimerControl() {
   const rest = useAppSelector(state => state.app.rest)
   const pomodoros = useAppSelector(state => state.app.pomodoros)
 
-  const [postHistoric, postResult] = usePostHistoricTaskMutation();
-  const userid = useAppSelector(state => state.auth.userid)
-  //Dispatch
   const dispatch = useAppDispatch()
 
-  //* Callback for adding a pomodoro to the historic and sending back end call.
-  const dispatchPomodoro = async () => {
-    const newHistoricTask = {
-      id: 0,
-      completeDate: todayDate,
-      time: settings.pomodoroDuration,
-      tag: actualTag!,
-    }
-    try {
-      const response = await postHistoric(
-        {
-          userId: userid!,
-          historicTask: newHistoricTask
-        }
-      ).unwrap()
-      console.log(response)
-      dispatch(addTimeEntry(todayDate, settings.pomodoroDuration * 60, actualTag!))
-    }
-    catch (error) {
-      console.log(error)
-    }
-  }
+  const [dispatchPomodoro] = useHandleCreateHistoric()
 
   const [progress, changeTimerState, resetTimer] = useTimerControl(dispatchPomodoro)
 
@@ -68,7 +41,7 @@ export default function TimerControl() {
 
   const onPomodoroEnd = () => {
     dispatchPomodoro()
-    dispatch(updateBlocks(actualIndex))
+    dispatch(updateBlocks(actualIndex!))
   }
 
   const handleWarningDialog = () => {
@@ -89,7 +62,7 @@ export default function TimerControl() {
         minutes={timerMinuts}
         seconds={timerSeconds}
         pomodoros={pomodoros}
-        longBreakEvery={settings.longBreakEvery} //Every long break
+        longBreakEvery={longBreakEvery} //Every long break
         progress={progress}
         rest={rest}
       ></TimerCard>
