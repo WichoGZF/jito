f9a618ddeda8ff57e660939883d564f1348246cf
WichoGZF <wichogzf@gmail.com>
2023-05-19 14:44:49 -0600

    Create task handler now only fetches on session

--- src/hooks/useHandleCreateTask.ts
+++ src/hooks/useHandleCreateTask.ts
@@ -6,28 +6,40 @@ import { setSnackbar, setSnackbarError } from "features/appSlice"
 import Task from 'types/Task'
 
 export default function useHandleCreateTask() {
-    const [postTask, {data, isSuccess, isError}] = usePostTaskMutation()
+    const [postTask, { data, isSuccess, isError }] = usePostTaskMutation()
     const userid = useAppSelector(state => state.auth.userid)
+    const hasSession = useAppSelector(state => state.auth.hasSession)
+    const maxTaskId = useAppSelector(state => state.tasks.tasks.reduce((max, task) => (task.id > max ? task.id : max), 0))
     const dispatch = useAppDispatch()
-    async function createTask(task: Task){
+    async function createTask(task: Task) {
         try {
-            const response = await postTask(
-                {
-                    userId: userid!,
-                    task: task
+            let newTask: Task
+            if (hasSession && (userid !== null)) {
+                const response = await postTask(
+                    {
+                        userId: userid!,
+                        task: task
+                    }
+                ).unwrap()
+                console.log(response)
+                newTask = {
+                    ...task,
+                    id: response
                 }
-            ).unwrap()
-            console.log(response)
-            dispatch(addTask({
-                ...task, 
-                id: response
-            }))
+            }else{
+                newTask = {
+                    ...task,
+                    id: maxTaskId + 1
+                }
+            }
+
+            dispatch(addTask(newTask))
             dispatch(setSnackbar("Task created!"))
         } catch (error) {
             console.log(error)
             dispatch(setSnackbarError("Error creating task"))
         }
     }
-    
+
     return [createTask]
 }
