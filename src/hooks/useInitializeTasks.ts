5bedfb7b931074aa33d1aa26d46bde72d87c444c
WichoGZF <wichogzf@gmail.com>
2023-05-19 15:39:30 -0600

    Initialize only iterates over no-repeatable tasks

--- src/hooks/useInitializeTasks.ts
+++ src/hooks/useInitializeTasks.ts
@@ -20,14 +20,17 @@ export default function useInitializeTasks(tasks: Task[], initialized: boolean):
         const todayDate = new Date
         todayDate.setHours(0, 0, 0, 0)
         tasks.forEach((task: Task, index: number) => {
-            const dateArray: string[] = task.date!.split('/');
-            const taskDate = new Date(parseInt(dateArray[2]), parseInt(dateArray[0]) - 1, parseInt(dateArray[1]))
-            let dayIsBefore;
-            dayIsBefore = isBefore(taskDate, todayDate)
+            if (task.repeat === 'no-repeat') {
+                console.log(task.date)
+                const dateArray: string[] = task.date!.split('/');
+                const taskDate = new Date(parseInt(dateArray[2]), parseInt(dateArray[0]) - 1, parseInt(dateArray[1]))
+                let dayIsBefore;
+                dayIsBefore = isBefore(taskDate, todayDate)
 
-            if (dayIsBefore) {
-                pastTasks.push(task)
+                if (dayIsBefore) {
+                    pastTasks.push(task)
 
+                }
             }
         })
 
