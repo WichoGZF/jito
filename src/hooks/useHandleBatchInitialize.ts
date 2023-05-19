bf27650cea28f0b8d45a30d6f5de70445699fa6b
WichoGZF <wichogzf@gmail.com>
2023-05-19 14:34:41 -0600

    batch initialize hook only fetches if session

--- src/hooks/useHandleBatchInitialize.ts
+++ src/hooks/useHandleBatchInitialize.ts
@@ -10,19 +10,22 @@ import format from "date-fns/format"
 export default function useHandleBatchInitialize() {
     const [postBatchInitialize] = usePostBatchInitializeMutation()
     const userId = useAppSelector((state) => state.auth.userid)
+    const hasSession = useAppSelector((state) => state.auth.hasSession)
     const dispatch = useAppDispatch()
 
     async function initializeTasks(initializeIds: number[], deleteIds: number[]): Promise<void> {
         const date = format(new Date(), "dd/LL/yy")
         try {
-            const response = await postBatchInitialize({
-                date: date,
-                userId: userId!,
-                updateIds: initializeIds,
-                deleteIds: deleteIds
+            if (hasSession && (userId !== null)) {
+                const response = await postBatchInitialize({
+                    date: date,
+                    userId: userId!,
+                    updateIds: initializeIds,
+                    deleteIds: deleteIds
 
-            }).unwrap()
-            console.log(response)
+                }).unwrap()
+                console.log(response)
+            }
             dispatch(updateDates(initializeIds))
             dispatch(deleteDue(deleteIds))
             dispatch(setSnackbar("Tasks initialized!"))
