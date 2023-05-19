7c71edc44fa2b3ee100c12bbeace48ae1670670b
WichoGZF <wichogzf@gmail.com>
2023-05-18 21:21:51 -0600

    removed unused params and updated dispatch

--- src/hooks/useHandleUpdateTask.ts
+++ src/hooks/useHandleUpdateTask.ts
@@ -6,7 +6,7 @@ import Task from 'types/Task'
 import { setSnackbar, setSnackbarError } from "features/appSlice"
 
 //Handles saving a task update
-export default function useHandleUpdateTask(index: number) {
+export default function useHandleUpdateTask() {
     const [updateTask, updateResult] = useUpdateTaskMutation()
     const userid = useAppSelector(state => state.auth.userid)
 
@@ -20,7 +20,8 @@ export default function useHandleUpdateTask(index: number) {
                     task: task
                 }
             ).unwrap()
-            dispatch(editTask(task, index))
+            console.log(response)
+            dispatch(editTask(task))
             dispatch(setSnackbar("Task updated!"))
         }
         catch (error) {
